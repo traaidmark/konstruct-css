4b7ba643edd84e7cfd243ef6daaabe5a743ab69a
Adrian Kirsten <adrian@wealthmigrate.com>
2021-11-23 10:40:24 +0200

    experimenting: story formats

---
+++ stories/parts/headings.stories.js
@@ -0,0 +1,8 @@
+export default {
+  title: 'stuff/Headings'
+};export const H1 = () => '<h1>Heading 1</h1>'
+export const H2 = () => '<h2>Heading 2</h2>'
+export const H3 = () => '<h3>Heading 3</h3>'
+export const H4 = () => '<h4>Heading 4</h4>'
+export const H5 = () => '<h5>Heading 5</h5>'
+export const H6 = () => '<h6>Heading 6</h6>'
